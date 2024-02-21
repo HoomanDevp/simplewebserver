27e57da8903c6f9359d151ab1dcd2a0a7bfca4df
m_yarahmadi <m_yarahmadi@modernisc.com>
2024-02-21 11:52:48 +0330

    Initial Commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const portName string = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Homepage")
+}
+func Info(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Info")
+}
+
+func main() {
+	log.Println("Started on port", portName)
+	fmt.Println("To close connection CTRL+C =)")
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/info", Info)
+
+	err := http.ListenAndServe(portName, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
